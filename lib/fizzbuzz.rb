ea5b25cc9a9f9461585b3d15e2b7406699d4204b
Haydon Houghton <haydonhoughton@outlook.com>
2020-01-08 16:13:47 +0000

    rewriting fizzbuzz as a method of integer

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,1 +1,13 @@
-#cleared repository
+class Integer
+  def fizzbuzz
+    if self % 3 == 0 && self % 5 == 0
+      return "fizzbuzz"
+    elsif self % 3 == 0
+      return "fizz"
+    elsif self % 5 == 0
+      return "buzz"
+    else
+      return self
+    end
+  end
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,1 +1,16 @@
-#clear spec file
+require 'fizzbuzz'
+describe 'fizzbuzz' do
+  it 'returns "fizz" for the number 3' do
+    expect(3.fizzbuzz).to eq 'fizz'
+  end
+  it 'returns "buzz" for the number 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+  it 'returns "4" for the number 4' do
+    expect(4.fizzbuzz).to eq 4
+  end
+  it 'returns "fizzbuzz" for the number 15' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
+  end
+
+end
